957bd5f4865b1001f0a83e8bb7846d6b40c7b5b9
Bodhikrishna <114943832+bodhikrishna@users.noreply.github.com>
2024-10-22 18:06:44 +0530

    Update Main.java
    updated main file , boundary conditions and testcases

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -11,7 +11,7 @@ public class Main {
     CountsBySoH counts = new CountsBySoH();
     //Looping through each of the battery's present capacity and further classifying based of SoH.
     for(int capacity: presentCapacities){
-      double SOH=100.0*capacity/120.0; //assumption that rated capacity=120Ah
+      double SOH=(100.0*capacity)/120.0; //assumption that rated capacity=120Ah
       if(SOH>83){
         counts.healthy++;
       }else if(SOH>=63){
@@ -33,15 +33,22 @@ public class Main {
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
 
+    int[] testCase1 = {110,88,40};
+    counts = countBatteriesByHealth(testCase1);
+    assert(counts.healthy == 1); //110 is healthy
+    assert(counts.exchange == 1); //88 is exchange
+    assert(counts.failed == 1); //40 is failed
+    System.out.println("Test Case 1 passed!");
+
+
     //testing for boundary conditions
     int[] boundaryCap={62,63,83,100};
     counts=countBatteriesByHealth(boundaryCap);
-
     //expected values for these edge cases are listed below
     assert(counts.healthy==1); //100 is healthy
-    assert(counts.exchange==2); //63 and 83 are in exchange
-    assert(counts.failed==1); //62 is failed
-    
+    assert(counts.exchange==1); //83 are in exchange
+    assert(counts.failed==2); //62 and 63 in failed
+    System.out.println("Boundary testcases passed");
     System.out.println("Done counting :)\n");
   }
 
